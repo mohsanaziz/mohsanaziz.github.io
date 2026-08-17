f2514a85c01c7cdb8fe20af25bae942e566fe4f6
Mohsan Aziz <49006366+mohsanaziz@users.noreply.github.com>
2026-08-17 11:23:00 +0200

    feat: migrate Astro and Tailwind (#14)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mohsanaziz.github.io',
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
